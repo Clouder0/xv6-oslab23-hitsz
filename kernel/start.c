89e3eef461dc27c407f12ff56e3600494f78a976
greenhandzpx <893522573@qq.com>
2023-09-06 15:10:54 +0800

    fix: remove pmp check && modify compile flags

--- kernel/start.c
+++ kernel/start.c
@@ -38,6 +38,11 @@ start()
   w_mideleg(0xffff);
   w_sie(r_sie() | SIE_SEIE | SIE_STIE | SIE_SSIE);
 
+  // configure Physical Memory Protection to give supervisor mode
+  // access to all of physical memory.
+  w_pmpaddr0(0x1fffffffffffffull);
+  w_pmpcfg0(0x1f);
+
   // ask for clock interrupts.
   timerinit();
 
