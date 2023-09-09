859956a0a5fcc4505d14c28e231dcd42a4e8ea02
greenhandzpx <893522573@qq.com>
2023-09-09 16:06:32 +0800

    feat(test): add wait test

--- user/sysinfotest.c
+++ user/sysinfotest.c
@@ -3,9 +3,7 @@
 #include "kernel/sysinfo.h"
 #include "user/user.h"
 
-
-void
-sinfo(struct sysinfo *info) {
+void sinfo(struct sysinfo *info) {
   if (sysinfo(info) < 0) {
     printf("FAIL: sysinfo failed");
     exit(1);
@@ -15,76 +13,71 @@ sinfo(struct sysinfo *info) {
 //
 // use sbrk() to count how many free physical memory pages there are.
 //
-int
-countfree()
-{
+int countfree() {
   uint64 sz0 = (uint64)sbrk(0);
   struct sysinfo info;
   int n = 0;
 
-  while(1){
-    if((uint64)sbrk(PGSIZE) == 0xffffffffffffffff){
+  while (1) {
+    if ((uint64)sbrk(PGSIZE) == 0xffffffffffffffff) {
       break;
     }
     n += PGSIZE;
   }
   sinfo(&info);
   if (info.freemem != 0) {
-    printf("FAIL: there is no free mem, but sysinfo.freemem=%d\n",
-      info.freemem);
+    printf("FAIL: there is no free mem, but sysinfo.freemem=%d\n", info.freemem);
     exit(1);
   }
   sbrk(-((uint64)sbrk(0) - sz0));
   return n;
 }
 
-void
-testmem() {
+void testmem() {
   struct sysinfo info;
   uint64 n = countfree();
-  
+
   sinfo(&info);
 
-  if (info.freemem!= n) {
+  if (info.freemem != n) {
     printf("FAIL: free mem %d (bytes) instead of %d\n", info.freemem, n);
     exit(1);
   }
-  
-  if((uint64)sbrk(PGSIZE) == 0xffffffffffffffff){
+
+  if ((uint64)sbrk(PGSIZE) == 0xffffffffffffffff) {
     printf("sbrk failed");
     exit(1);
   }
 
   sinfo(&info);
-    
-  if (info.freemem != n-PGSIZE) {
-    printf("FAIL: free mem %d (bytes) instead of %d\n", n-PGSIZE, info.freemem);
+
+  if (info.freemem != n - PGSIZE) {
+    printf("FAIL: free mem %d (bytes) instead of %d\n", n - PGSIZE, info.freemem);
     exit(1);
   }
-  
-  if((uint64)sbrk(-PGSIZE) == 0xffffffffffffffff){
+
+  if ((uint64)sbrk(-PGSIZE) == 0xffffffffffffffff) {
     printf("sbrk failed");
     exit(1);
   }
 
   sinfo(&info);
-    
+
   if (info.freemem != n) {
     printf("FAIL: free mem %d (bytes) instead of %d\n", n, info.freemem);
     exit(1);
   }
 }
 
-void
-testcall() {
+void testcall() {
   struct sysinfo info;
-  
+
   if (sysinfo(&info) < 0) {
     printf("FAIL: sysinfo failed\n");
     exit(1);
   }
 
-  if (sysinfo((struct sysinfo *) 0xeaeb0b5b00002f5e) !=  0xffffffffffffffff) {
+  if (sysinfo((struct sysinfo *)0xeaeb0b5b00002f5e) != 0xffffffffffffffff) {
     printf("FAIL: sysinfo succeeded with bad argument\n");
     exit(1);
   }
@@ -95,34 +88,32 @@ void testproc() {
   uint64 nproc;
   int status;
   int pid;
-  
+
   sinfo(&info);
   nproc = info.nproc;
 
   pid = fork();
-  if(pid < 0){
+  if (pid < 0) {
     printf("sysinfotest: fork failed\n");
     exit(1);
   }
-  if(pid == 0){
+  if (pid == 0) {
     sinfo(&info);
-    if(info.nproc != nproc+1) {
-      printf("sysinfotest: FAIL nproc is %d instead of %d\n", info.nproc, nproc+1);
+    if (info.nproc != nproc + 1) {
+      printf("sysinfotest: FAIL nproc is %d instead of %d\n", info.nproc, nproc + 1);
       exit(1);
     }
     exit(0);
   }
   wait(&status, 0);
   sinfo(&info);
-  if(info.nproc != nproc) {
-      printf("sysinfotest: FAIL nproc is %d instead of %d\n", info.nproc, nproc);
-      exit(1);
+  if (info.nproc != nproc) {
+    printf("sysinfotest: FAIL nproc is %d instead of %d\n", info.nproc, nproc);
+    exit(1);
   }
 }
 
-int
-main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   printf("sysinfotest: start\n");
   testcall();
   testmem();
---
+++ user/waittest.c
@@ -0,0 +1,34 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+const int nonblock = 1;
+
+void waittest(void) {
+    printf("wait test\n");
+
+    int pid = fork();
+    if (pid == 0) {
+        sleep(10);
+        exit(0);
+    }
+
+    for (int i = 0; i < 3; i++) {
+        if (wait(0, nonblock) < 0) {
+            printf("no child exited yet, round %d\n", i);
+        } else {
+            printf("wait shouldn't block!\n");
+            printf("wait test FAIL\n");
+            exit(1);
+        }
+    }
+    int child_pid = wait(0, 0);
+    printf("child exited, pid %d\n", child_pid);
+
+    printf("wait test OK\n");
+}
+
+int main(int argc, char *argv[]) {
+    waittest();
+    exit(0);
+}
